3e0621f57f54c411a56daf73d0b7337330292493
Ryan Thames <ryanthames@gmail.com>
2017-01-12 15:31:59 -0600

    Metex GenServer

--- metex/lib/worker.ex
+++ metex/lib/worker.ex
@@ -1,23 +1,73 @@
 defmodule Metex.Worker do
-  def loop do
-    receive do
-      {sender_pid, location} ->
-        send(sender_pid, {:ok, temperature_of(location)})
-      _ ->
-        IO.puts "don't know how to process this message"
-    end
-    loop
+  use GenServer
+
+  @name MW
+
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: MW])
   end
 
-  def temperature_of(location) do
-    result = url_for(location) |> HTTPoison.get |> parse_response
+  def get_temperature(location) do
+    GenServer.call(@name, {:location, location})
+  end
 
-    case result do
+  def get_stats do
+    GenServer.call(@name, :get_stats)
+  end
+
+  def reset_stats do
+    GenServer.cast(@name, :reset_stats)
+  end
+
+  def stop do
+    GenServer.cast(@name, :stop)
+  end
+
+  ## Server Callbacks
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
       {:ok, temp} ->
-        "#{location}: #{temp} C"
-      :error ->
-        "#{location} not found"
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp} C", new_stats}
+      _ ->
+        {:reply, :error, stats}
     end
+  end
+
+  def handle_call(:get_stats, _from, stats) do
+    {:reply, stats, stats}
+  end
+
+  def handle_cast(:reset_stats, _stats) do
+    {:noreply, %{}}
+  end
+
+  def handle_cast(:stop, stats) do
+    {:stop, :normal, stats}
+  end
+
+  def handle_info(msg, stats) do
+    IO.puts "received #{inspect msg}"
+    {:noreply, stats}
+  end
+
+  def terminate(reason, stats) do
+    IO.puts "server terminated because of #{inspect reason}"
+    inspect stats
+    :ok
+  end
+
+  ## Helper Functions
+
+  def temperature_of(location) do
+    url_for(location) |> HTTPoison.get |> parse_response
   end
 
   defp url_for(location) do
@@ -44,5 +94,14 @@ defmodule Metex.Worker do
 
   defp apikey do
     "58cc58b739454be847bb389852959eed"
+  end
+
+  defp update_stats(old_stats, location) do
+    case Map.has_key?(old_stats, location) do
+      true ->
+        Map.update!(old_stats, location, &(&1 + 1))
+      false ->
+        Map.put_new(old_stats, location, 1)
+    end
   end
 end
